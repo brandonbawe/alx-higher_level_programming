dac902320cfa9042b6aace320885909ce15d60a1
brandonbawe <dbrandonbawe@gmail.com>
2023-01-24 22:15:58 +0100

    Task 0 redo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    printed = 0
-    if(my_list):
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end='')
-                printed += 1
-            except IndexError:
-                print()
-                return(printed)
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except:
+            break
+        else:
+            count += 1
+
     print()
-    return(printed)
+    return (count)
